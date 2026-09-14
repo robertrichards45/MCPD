f5bb14acfa984fea4c7f446f9665d94c08df9076
robertrichards45 <robertrichards45@gmail.com>
2026-09-14 05:15:44 -0400

    Register actual official form PDF preview routes

--- app/routes/fto_refinements.py
+++ app/routes/fto_refinements.py
@@ -377,6 +377,7 @@ from . import scenario_evidence as _scenario_evidence
 from . import scenario_shift as _scenario_shift
 from . import scenario_notebook as _scenario_notebook
 from . import scenario_paperwork as _scenario_paperwork
+from . import scenario_official_forms as _scenario_official_forms
 from . import scenario_analytics as _scenario_analytics
 from . import scenario_instructor as _scenario_instructor
 bp.register_blueprint(_scenario_lab.bp)
@@ -384,5 +385,6 @@ bp.register_blueprint(_scenario_evidence.bp)
 bp.register_blueprint(_scenario_shift.bp)
 bp.register_blueprint(_scenario_notebook.bp)
 bp.register_blueprint(_scenario_paperwork.bp)
+bp.register_blueprint(_scenario_official_forms.bp)
 bp.register_blueprint(_scenario_analytics.bp)
 bp.register_blueprint(_scenario_instructor.bp)
